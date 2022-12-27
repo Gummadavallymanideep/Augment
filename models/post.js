f79a14e5e6291b2f5d9fa2d47da7067b4d729455
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2022-12-27 20:57:09 +0530

    social network

--- models/post.js
+++ models/post.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    content:{
+    content: {
         type: String,
         required: true
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    },
-    comment: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
+    }
 },{
     timestamps: true
 });
 
 const Post = mongoose.model('Post', postSchema);
-module.exports = Post;
+module.exports = Post; 
