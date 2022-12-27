2eeb35a26dcef81aeb5749eb5f2309437274bc5e
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2022-12-27 07:10:29 +0530

    social network

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = 8003;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongodb-session')(session);
 
 app.use(express.urlencoded());
 
@@ -30,7 +30,7 @@ app.set('views', './views');
 
 // mongo store is used to store the session cookie in the db
 app.use(session({
-    name: 'codeial',
+    name: 'authentication',
     // TODO change the secret before deployment in production mode
     secret: 'blahsomething',
     saveUninitialized: false,
@@ -38,7 +38,7 @@ app.use(session({
     cookie: {
         maxAge: (1000 * 60 * 100)
     },
-    store: new MongoStore(
+    store: MongoStore.create(
         {
             mongooseConnection: db,
             autoRemove: 'disabled'
